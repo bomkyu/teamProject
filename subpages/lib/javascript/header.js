de9dac9337481b9d4c0bb8b008d33f351a53eee3
bomkyu <a01090762806@gmail.com>
2023-04-14 16:41:10 +0900

    헤더 스크립트 취합

--- subpages/lib/javascript/header.js
+++
@@ -1,9 +0,0 @@
-const Elheader = document.querySelector('header');
-
-window.addEventListener('scroll',function(){
-    if(this.window.scrollY > 0){
-        Elheader.classList.add('scroll')
-    }else{
-        Elheader.classList.remove('scroll')
-    }
-})
