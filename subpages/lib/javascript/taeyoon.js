5ef175a456a199fdb067085a212c98455fb4059b
2taeyoon <2tae@kakao.com>
2023-04-17 05:11:05 +0900

    contact page working
    contact page working

--- subpages/lib/javascript/taeyoon.js
+++ subpages/lib/javascript/taeyoon.js
@@ -1,9 +1,26 @@
 const tabs = document.querySelectorAll('.tab > li');
-tabs.forEach((tab)=>{
-    tab.addEventListener('click',()=>{
-        tabs.forEach((tab)=>{
+const cardFronts = document.querySelectorAll('.card_front');
+const cardBacks = document.querySelectorAll('.card_back');
+const cardWraps = document.querySelectorAll('.card_wrap');
+
+tabs.forEach((tab, index) => {
+    tab.addEventListener('click', () => {
+        tabs.forEach((tab) => {
             tab.classList.remove('active');
         });
+        cardFronts.forEach((cardFront) => {
+            cardFront.classList.remove('active');
+        });
+        cardBacks.forEach((cardBack) => {
+            cardBack.classList.remove('active');
+        });
+        cardWraps.forEach((cardWrap) => {
+            cardWrap.classList.remove('active');
+        });
+
         tab.classList.add('active');
+        cardFronts[index].classList.add('active');
+        cardBacks[index].classList.add('active');
+        cardWraps[index].classList.add('active');
     });
 });
