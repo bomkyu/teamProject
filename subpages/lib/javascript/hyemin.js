1f5da1f71f5758a56f01dd1241fc7fc50e7c376e
HYEMIN JO <gkdlt7373@gmail.com>
2023-04-18 20:12:26 +0900

    faq javascript 추가
    faq javascript 추가

--- subpages/lib/javascript/hyemin.js
+++ subpages/lib/javascript/hyemin.js
@@ -1,3 +1,4 @@
+//타이핑 효과//
 let i = 0;
 let txt = 'Ask Your Developers'; 
 let speed = 100;
@@ -10,3 +11,24 @@ function typing() {
   }
 }
 typing();
+
+//답변 펼치기//
+const faqBar = document.querySelectorAll('.faq_bar_wrap > li');
+console.log(faqBar);
+const faqAnswer = document.querySelectorAll('.faq_answer_wrap > li');
+console.log(faqAnswer);
+
+
+faqBar.forEach(function(item,index){
+  item.addEventListener('click',function(){
+    if(faqAnswer[index].classList.contains('open')){
+      faqAnswer[index].classList.remove('open');
+    }else{
+      faqAnswer.forEach(function(answer){
+        answer.classList.remove('open');
+      })
+      faqAnswer[index].classList.add('open');
+    }
+  })
+})
+
